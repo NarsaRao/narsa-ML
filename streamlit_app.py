fda11b2a7b70eb7b068942599f8eeeff644b3b94
NarsaRao <narsaraoindana@gmail.com>
2025-02-19 11:59:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,9 @@ import pandas as pd
 st.title('Machine Learning App')
 
 st.write('A Basic ML model Builder')
-df=pd.read_csv('https://raw.githubusercontent.com/NarsaRao/narsa-ML/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('DATA'):
+  st.write('**Raw Data**')
+  df=pd.read_csv('https://raw.githubusercontent.com/NarsaRao/narsa-ML/refs/heads/master/penguins_cleaned.csv')
+  df
+  
+  
