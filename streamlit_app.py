35c784776807a2b135a351717d700dc8920e05fa
NarsaRao <narsaraoindana@gmail.com>
2025-02-19 12:02:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,11 @@ with st.expander('DATA'):
   st.write('**Raw Data**')
   df=pd.read_csv('https://raw.githubusercontent.com/NarsaRao/narsa-ML/refs/heads/master/penguins_cleaned.csv')
   df
+  st.write('**X**')
+  x=df.drop('species',axis=1)
+  x
+  st.write('**Y**')
+  y=df.species
+  y
   
   
