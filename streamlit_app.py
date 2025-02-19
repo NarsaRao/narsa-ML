6ea01e73ed45e7c02898ae28f76dcca8cdf5b46a
NarsaRao <narsaraoindana@gmail.com>
2025-02-19 11:54:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
+st.title('Machine Learning App')
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.write('A Basic ML model Builder')
+df=pd.read_csv('https://raw.githubusercontent.com/NarsaRao/narsa-ML/refs/heads/master/penguins_cleaned.csv')
+df
